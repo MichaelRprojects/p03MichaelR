24d18e0282e08a7e2f5304138fea7cffa6597c33
MichaelRprojects <msr170930@utdallas.edu>
2021-05-03 15:10:07 -0500

    simplistic wall jump
    simplified wall jump

--- Assets/Scripts/PMovementP3.cs
+++ Assets/Scripts/PMovementP3.cs
@@ -18,16 +18,27 @@ public class PMovementP3 : MonoBehaviour
     float speed = 8f;
     float runspeed = 14f;
 
+    //Vector3 recmove;
+    Vector3 lmove;
+    Vector3 move;
+    //bool cmove = true;
+
     float wallspeed = 10f;
 
     bool stopup = true;
     public static bool pwrtilt = false;
+
+    public static Vector3 walldir;
+    public static Vector3 walldir2;
+
 
     //was-9.8
     float gravity = -22.8f;
     //was 3
     float jumpHeight = 6.5f;
     int jumpnum = 2;
+    bool rwalljumpa = false;
+    bool rwjmping = false;
     float airslow = 1f;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -39,6 +50,19 @@ public class PMovementP3 : MonoBehaviour
     void Start()
     {
 
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //!isgrounded
+        if (!isGrounded)
+        {
+            if (WallD.pisRWallrun == true && hit.normal.y < .1f)
+            {
+                walldir2 = hit.normal;
+                Debug.DrawRay(hit.point, walldir2, Color.red, 2.5f);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -54,18 +78,32 @@ public class PMovementP3 : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        //if (WallD.pisRWallrun == false)
+        //{
+        //if (isGrounded)
+        //{
+            Vector3 move = transform.right * x + transform.forward * z;
+        //}
+        //if (!isGrounded)
+        //{
+            //Vector3 move = transform.right * x + transform.forward * z + lmove;
+        //}
 
-        //controller.Move(move * speed * Time.deltaTime);
-        controller.Move(move * (speed/airslow) * Time.deltaTime);
+        controller.Move(move * (speed / airslow) * Time.deltaTime);
+        //}
+        //if (WallD.pisRWallrun == true)
+        //{
+        //Vector3 move = transform.forward * z;
+        //controller.Move(move * (speed / airslow) * Time.deltaTime);
+        //}
+        //}
+
+        //Debug.DrawRay(hit.point, hit.normal, Color.red, 2.5f);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            //velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            //if (jumpnum >= 2)
-            //{
-                velocity += Mathf.Sqrt(jumpHeight * -2f * gravity) * Vector3.up;
-            //}
+            //Vector3 lmove = move;
+            velocity += Mathf.Sqrt(jumpHeight * -2f * gravity) * Vector3.up;
             jumpnum = 1;
         }
         if (Input.GetButtonDown("Jump") && !isGrounded)
@@ -74,25 +112,57 @@ public class PMovementP3 : MonoBehaviour
             {
                 velocity.y = 0;
                 velocity += Mathf.Sqrt(jumpHeight * -2f * gravity) * Vector3.up;
+
+                if (rwalljumpa == true)
+                {
+                    rwjmping = true;
+                    //velocity += Mathf.Sqrt(jumpHeight * -2f * gravity) * -Vector3.right;
+                    //controller.Move(-transform.right * 400f * Time.deltaTime);
+                }
+
                 jumpnum = 0;
             }
+        }
+        if (rwjmping == true)
+        {
+            controller.Move(walldir2 * speed * 6f * Time.deltaTime);
+            controller.Move(transform.forward * 18f * Time.deltaTime);
+            controller.Move(transform.up * 18f * Time.deltaTime);
+            DlHlpr.DelayAction(this, rwjmpingf, .25f);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
+        //recmove = move;
+
         if(isGrounded)
         {
             airslow = 1f;
             jumpnum = 2;
             pwrtilt = false;
+            stopup = true;
+            rwalljumpa = false;
+            Vector3 lmove = move;
+            //if (cmove == true)
+            //{
+                //movelm();
+                //DlHlpr.DelayAction(this, cmovet, 1f);
+            //}
+            //Debug.Log("lmmmmmmmm:" + lmove);
+            //Debug.Log("m:" + move);
+            //Debug.Log("rrrrrrm:" + recmove);
+            //Debug.Log("lm:" + lmove);
         }
 
         if (!isGrounded)
         {
             //was 2 1.75
             airslow = 1.4f;
+            //Debug.Log("lm:" + lmove);
+            //Debug.Log("m:" + move);
+            //controller.Move(lmove * (speed / airslow) * 120f * Time.deltaTime);
         }
 
         if (isGrounded && velocity.y < 0)
@@ -117,22 +187,93 @@ public class PMovementP3 : MonoBehaviour
             speed = 12f;
         }
 
+
+
         if (WallD.pisRWallrun == true)
         {
             if (!isGrounded)
             {
                 pwrtilt = true;
+                //rwalljumpa = false;
+
+                //Vector3 Lerpdirec = Vector3.Lerp(transform.right, walldir.normalized, 2f * Time.deltaTime);
+                //*transform.rot
+                //transform.rotation = Quaternion.FromToRotation(Vector3.right, Lerpdirec);
+
+                //transform.rotation = Quaternion.FromToRotation(Vector3.right, Lerpdirec);
+
+                //float pang = Vector3.Angle(walldir.normalized, transform.rotation);
+
+                //Debug.Log(wallang);
+
+                //Vector3 move2 = walldir.normalized * 10f * Time.deltaTime;
+
+                //controller.Move(wallang * 20f * Time.deltaTime);
+
+                //transform.localRotation = Quaternion.Euler(walldir.normalized);
                 //if (velocity)
                 if (Input.GetAxis("Vertical") > 0)
                 {
+                    if (stopup == true)
+                    {
+                        velocity.y = 0;
+                        stopup = false;
+                    }
+
+                    jumpnum = 2;
+
+                    rwalljumpa = true;
+
                     controller.Move(transform.forward * wallspeed * Time.deltaTime);
+                    //24
+                    controller.Move(transform.right * 24f * Time.deltaTime);
+
+                    //if (walldir.normalized.x > 0)
+                    //{
+                    //Vector3 Lerpdirec = Vector3.Lerp(transform.right, walldir.normalized, 20f * Time.deltaTime);
+                    //transform.rotation = Quaternion.FromToRotation(Vector3.right, Lerpdirec);
+                    //Vector3 Lerpdirec = Vector3.Lerp(transform.right, walldir.normalized, 2f * Time.deltaTime);
+                    //transform.rotation = Quaternion.FromToRotation(Vector3.right, Lerpdirec);
+                    //transform.rotation = Quaternion.FromToRotation(-Vector3.right, walldir.normalized);
+                    //transform.rotation = Quaternion.Slerp(transform.rotation, walldir.normalized, 20f * Time.deltaTime);
+                    //}
+                    if (walldir.normalized.x < 0)
+                    {
+                        //transform.rotation = Quaternion.FromToRotation(Vector3.right, walldir.normalized);
+                    }
+                        //Vector3 wallang = Vector3.Cross(walldir.normalized, Vector3.up);
+                        //controller.Move(transform.rotate.wallang);
+                        //float wallangx = wallang.x;
+                        //transform.rotation = Quaternion.FromToRotation(Vector3.right, wallang);
+                        //transform.localEulerAngles = new Vector3(wallang);
+                        //Debug.Log("W:" + wallang);
+                        //if (wallang.x > -1)
+                        //{
+                        //Vector3 Lerpdirec = Vector3.Lerp(-transform.right, wallang, 2f * Time.deltaTime);
+                        //Debug.Log("gL:" + Lerpdirec);
+                        //transform.rotation = Quaternion.FromToRotation(Vector3.right, Lerpdirec);
+                        //}
+                        //if (wallang.x < -1)
+                        //{
+                        //Vector3 Lerpdirec = Vector3.Lerp(transform.right, wallang, 2f * Time.deltaTime);
+                        //Debug.Log("lL:" + Lerpdirec);
+                        //transform.rotation = Quaternion.FromToRotation(Vector3.right, Lerpdirec);
+                        //}
+
+                        //Debug.Log(wallang);
+                        //Debug.Log(Lerpdirec);
+                    if (velocity.y == 0)
+                    {
+                        //.8
+                        gravity = -1.8f;
+                    }
                 }
-                if (stopup == true)
+                if (velocity.y <= 0)
                 {
-                    velocity.y = 0;
-                    stopup = false;
+                    //.8
+                    gravity = -1.8f;
                 }
-                gravity = -.8f;
+
             }
         }
         if (WallD.pisRWallrun == false)
@@ -140,10 +281,26 @@ public class PMovementP3 : MonoBehaviour
             pwrtilt = false;
             stopup = true;
             gravity = -22.8f;
+            rwalljumpa = false;
         }
     }
     public void Setspeed2()
     {
         speed = runspeed;
+    }
+    void rwjmpingf()
+    {
+        rwjmping = false;
+    }
+    void movelm()
+    {
+        //Vector3 lmove = recmove;
+        //Debug.Log("lm:" + lmove);
+        //cmove = false;
+        //DlHlpr.DelayAction(this, cmovet, 1f);
+    }
+    void cmovet()
+    {
+        //cmove = true;
     }
 }
--- Assets/Scripts/WallD.cs
+++ Assets/Scripts/WallD.cs
@@ -8,6 +8,17 @@ public class WallD : MonoBehaviour
     Collider ocollider;
     float size;
     float rotyw;
+    bool chwdir = false;
+    public bool isfright;
+    public bool isbright;
+    public bool iscenter;
+    public static bool centerd = false;
+    public static bool fcenterd = false;
+    public static bool bcenterd = false;
+    //public int directnum;
+    [SerializeField] Transform Wallch;
+    [SerializeField] Transform Wallche;
+    [SerializeField] LayerMask Ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,21 +38,65 @@ public class WallD : MonoBehaviour
             //Debug.Log(rotyw);
             if (ocollider.bounds.size.y > 1.1f)
             {
-                if(other.transform.rotation.eulerAngles.y == 0)
-                {
+                chwdir = true;
+                //if(other.transform.rotation.eulerAngles.y == 0)
+                //{
                     pisRWallrun = true;
-                }
+                //}
             }
         }
     }
     private void OnTriggerExit(Collider other)
     {
+        chwdir = false;
         pisRWallrun = false;
+        //if (iscenter == true)
+        //{
+            //centerd = false;
+        //}
+        //if (isfright == true)
+        //{
+            //fcenterd = false;
+        //}
+        //if (isbright == true)
+        //{
+            //bcenterd = false;
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (chwdir == true)
+        {
+            //*2.5 1f end .pos
+            //Debug.DrawRay(Wallch.position, Wallche.position - Wallch.position, Color.blue, 2.5f);
+            RaycastHit wallh;
+            //lpos *2.5
+            Physics.Raycast(Wallch.position, Wallche.position - Wallch.position, out wallh, 2.5f, Ground);
+
+            if (wallh.transform != null)
+            {
+                //if(iscenter == true)
+                //{
+                    //centerd = true;
+                //}
+                //if (isfright == true)
+                //{
+                    //fcenterd = true;
+                //}
+                //if (isbright == true)
+                //{
+                    //bcenterd = true;
+                //}
+                PMovementP3.walldir += wallh.normal;
+                //Debug.DrawRay(wallh.point, PMovementP3.walldir.normalized, Color.green, 2.5f);
+                //Debug.DrawRay(wallh.point, PMovementP3.walldir.normalized, Color.red, 2.5f);
+            }
+
+            //Debug.DrawRay(Wallch.position, Wallch.transform.right * 2.5f, Color.blue, 1f);
+            //RaycastHit pftyfvdeg;
+            //Physics.Raycast(Wallch.position, Wallch.transform.right, out pftyfvdeg, 2.5f, Ground);
+        }
     }
 }
